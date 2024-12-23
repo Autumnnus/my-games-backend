507f224198a6cfeb865dc851b10843868bda5af9
Kadir <84664117+VectortheGreat@users.noreply.github.com>
2024-12-23 21:37:48 +0300

    auth done

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,14 +1,9 @@
-import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
 import type { NextFunction, Request, Response } from "express";
 import asyncErrorWrapper from "express-async-handler";
-import nodemailer from "nodemailer";
 import { generateAccessToken } from "../helpers/auth/jwt-helper";
 import CustomError from "../helpers/errors/CustomError";
-import sendEmail from "../helpers/functions/sendMail";
-import { comparePassword } from "../helpers/input/inputHelpers";
 import { createResponse } from "../middlewares/error/CreateResponse";
-import User from "../models/User";
 import authService from "../services/auth.service";
 dotenv.config();
 
@@ -23,7 +18,6 @@ const registerController = asyncErrorWrapper(
     const { email, name, password } = req.body;
     try {
       const newUser = await authService.registerService(email, name, password);
-      // res.status(200).json(createResponse(game));
       generateAccessToken(newUser, res);
     } catch (error) {
       res.status(404).json(createResponse(null, false, `Error: ${error}`));
@@ -36,7 +30,6 @@ const loginController = asyncErrorWrapper(
     const { email, password } = req.body;
     try {
       const user = await authService.loginService(email, password);
-      // res.status(200).json(createResponse(game));
       generateAccessToken(user, res);
     } catch (error) {
       res.status(404).json(createResponse(null, false, `Error: ${error}`));
@@ -77,169 +70,61 @@ const forgotPasswordController = asyncErrorWrapper(
   }
 );
 
-const resetPassword = asyncErrorWrapper(
-  async (req: Request, res: Response, next: NextFunction) => {
+const resetPasswordController = asyncErrorWrapper(
+  async (req: Request, res: Response) => {
     const { resetPasswordToken } = req.query;
     const { password } = req.body;
     try {
-      if (!resetPasswordToken) {
-        return next(new CustomError("Please enter a valid token", 400));
-      }
-      const user = await User.findOne({
-        resetPasswordToken: resetPasswordToken,
-        resetPasswordExpire: { $gt: Date.now() }
-      });
-      if (!user) {
-        return next(new CustomError("Invalid Token or Session Expired", 400));
-      }
-      user.password = password;
-      user.resetPasswordToken = undefined;
-      user.resetPasswordExpire = undefined;
-      await user.save();
-      return res.status(200).json({
-        success: true,
-        message: "Reset Password Process Success"
-      }) as never;
+      const data = await authService.resetPasswordService(
+        resetPasswordToken as string,
+        password
+      );
+      res.status(200).json(createResponse(data));
     } catch (error) {
       console.error("ERROR: ", error);
-      return next(new CustomError(`Error: ${error}`, 404));
+      res.status(404).json(createResponse(null, false, `Error: ${error}`));
     }
   }
 );
 
-const editUser = asyncErrorWrapper(
-  async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+const editUserController = asyncErrorWrapper(
+  async (req: AuthenticatedRequest, res: Response) => {
     const editInformation = req.body;
+    const userId = req.user?.id;
     try {
-      // const user = await findUserByIdOrError(req.user?.id ||"", next);
-      const user = await User.findById(req.user?.id);
-      if (!user) {
-        return next(new CustomError("User not found", 404));
-      }
-      //? IF NO CHANGES
-      // if (editInformation.email && editInformation.email === user.email) {
-      //   return res
-      //     .status(400)
-      //     .json({ success: false, message: "No changes detected email" });
-      // }
-      if (editInformation.name && editInformation.name === user?.name) {
-        return res.status(400).json({
-          success: false,
-          message: "No changes detected name"
-        }) as never;
-      }
-      if (
-        editInformation.password &&
-        comparePassword(editInformation.password, user.password)
-      ) {
-        return res.status(400).json({
-          success: false,
-          message: "No changes detected password"
-        }) as never;
-      }
-      if (
-        editInformation.profileImage &&
-        editInformation.profileImage === user.profileImage
-      ) {
-        return res.status(400).json({
-          success: false,
-          message: "No changes detected profileImage"
-        }) as never;
-      }
-      //? EDIT
-      // if (editInformation.email) {
-      //   user.email = editInformation.email;
-      // }
-      if (editInformation.name) {
-        user.name = editInformation.name;
-      }
-      if (editInformation.password) {
-        const salt = await bcrypt.genSalt(10);
-        editInformation.password = await bcrypt.hash(
-          editInformation.password,
-          salt
-        );
-      }
-      if (editInformation.profileImage) {
-        user.profileImage = editInformation.profileImage;
-      }
-
-      const updatedUser = await User.findByIdAndUpdate(
-        req.user?.id,
+      const data = await authService.editUserService(
         editInformation,
-        {
-          new: true,
-          runValidators: true
-        }
+        userId as string
       );
-      return res.status(200).json({
-        success: true,
-        message: JSON.stringify(editInformation),
-        data: updatedUser
-      }) as never;
+      res.status(200).json(createResponse(data));
     } catch (error) {
       console.error("ERROR: ", error);
-      return next(new CustomError(`Error: ${error}`, 404));
+      res.status(404).json(createResponse(null, false, `Error: ${error}`));
     }
   }
 );
 
-const validateEmail = asyncErrorWrapper(
-  async (req: Request, res: Response, next: NextFunction) => {
+const validateEmailController = asyncErrorWrapper(
+  async (req: Request, res: Response) => {
     const resetEmail = req.body.email;
-    const user = await User.findOne({ email: resetEmail });
-    if (!user) {
-      return next(new CustomError("There is no user with that e-mail.", 400));
-    }
-    if (user.isVerified) {
-      return next(new CustomError("This user already verified", 400));
-    }
-
-    const verificationToken = user.getVerificationTokenFromUser();
-    await user.save();
-    const verifyAccountUrl = `${process.env.FRONTEND_URL}/api/auth/verifyAccount?verificationToken=${verificationToken}`;
 
     try {
-      const info = await sendEmail(
-        user,
-        "Verify Account",
-        "We received a request to verify the email for your account. If you did not request this change, please ignore this email. No changes will be made to your account.",
-        verifyAccountUrl
-      );
-
-      return res.status(200).json({
-        msg: "Email Sent",
-        info: info,
-        preview: nodemailer.getTestMessageUrl(info)
-      }) as never;
+      const data = await authService.validateEmailService(resetEmail);
+      res.status(200).json(createResponse(data));
     } catch (err) {
-      return next(err);
+      res.status(404).json(createResponse(null, false, `Error: ${err}`));
     }
   }
 );
 
-const verifyAccount = asyncErrorWrapper(
+const verifyAccountController = asyncErrorWrapper(
   async (req: Request, res: Response, next: NextFunction) => {
     const { verificationToken } = req.query;
     try {
-      if (!verificationToken) {
-        return next(new CustomError("Invalid token", 400));
-      }
-      const user = await User.findOne({
-        verificationToken: verificationToken,
-        verificationExpire: { $gt: Date.now() }
-      });
-      if (!user) {
-        return next(new CustomError("Invalid Token or Session Expired", 400));
-      }
-      user.isVerified = true;
-      user.verificationToken = undefined;
-      user.verificationExpire = undefined;
-      await user.save();
-      return res.status(200).json({
-        success: true,
-        message: "Account Verification Process Success"
-      }) as never;
+      const data = await authService.verifyAccountService(
+        verificationToken as string
+      );
+      res.status(200).json(createResponse(data));
     } catch (error) {
       console.error("ERROR: ", error);
       return next(new CustomError(`Error: ${error}`, 404));
@@ -248,12 +133,12 @@ const verifyAccount = asyncErrorWrapper(
 );
 
 export {
-  editUser,
-  forgotPasswordController as forgotPassword,
-  loginController as login,
-  logoutController as logout,
-  registerController as register,
-  resetPassword,
-  validateEmail,
-  verifyAccount
+  editUserController,
+  forgotPasswordController,
+  loginController,
+  logoutController,
+  registerController,
+  resetPasswordController,
+  validateEmailController,
+  verifyAccountController
 };
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import bcrypt from "bcryptjs";
 import nodemailer from "nodemailer";
 import sendEmail from "../helpers/functions/sendMail";
 import {
@@ -31,6 +32,7 @@ async function loginService(email: string, password: string) {
   if (!user) {
     throw new Error("User not found");
   }
+
   return user;
 }
 
@@ -58,8 +60,131 @@ async function forgotPasswordService(email: string) {
   };
 }
 
+async function resetPasswordService(
+  resetPasswordToken: string,
+  password: string
+) {
+  if (!resetPasswordToken) {
+    throw new Error("Please enter a valid token");
+  }
+  const user = await User.findOne({
+    resetPasswordToken: resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() }
+  });
+  if (!user) {
+    throw new Error("Invalid Token or Session Expired");
+  }
+  user.password = password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+  await user.save();
+  return {
+    success: true,
+    message: "Reset Password Process Success"
+  };
+}
+
+async function editUserService(editInformation: any, userId: string) {
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new Error("User not found");
+  }
+  //? IF NO CHANGES
+  // if (editInformation.email && editInformation.email === user.email) {
+  //   return res
+  //     .status(400)
+  //     .json({ success: false, message: "No changes detected email" });
+  // }
+  if (editInformation.name && editInformation.name === user?.name) {
+    throw new Error("No changes detected name");
+  }
+  if (
+    editInformation.password &&
+    comparePassword(editInformation.password, user.password)
+  ) {
+    throw new Error("No changes detected password");
+  }
+  if (
+    editInformation.profileImage &&
+    editInformation.profileImage === user.profileImage
+  ) {
+    throw new Error("No changes detected profileImage");
+  }
+  //? EDIT
+  // if (editInformation.email) {
+  //   user.email = editInformation.email;
+  // }
+  if (editInformation.name) {
+    user.name = editInformation.name;
+  }
+  if (editInformation.password) {
+    const salt = await bcrypt.genSalt(10);
+    editInformation.password = await bcrypt.hash(
+      editInformation.password,
+      salt
+    );
+  }
+  if (editInformation.profileImage) {
+    user.profileImage = editInformation.profileImage;
+  }
+
+  const updatedUser = await User.findByIdAndUpdate(userId, editInformation, {
+    new: true,
+    runValidators: true
+  });
+  return updatedUser;
+}
+
+async function validateEmailService(email: string) {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("There is no user with that e-mail.");
+  }
+  if (user.isVerified) {
+    throw new Error("This account is already verified.");
+  }
+
+  const verificationToken = user.getVerificationTokenFromUser();
+  await user.save();
+  const verifyAccountUrl = `${process.env.FRONTEND_URL}/api/auth/verifyAccount?verificationToken=${verificationToken}`;
+  const info = await sendEmail(
+    user,
+    "Verify Account",
+    "We received a request to verify the email for your account. If you did not request this change, please ignore this email. No changes will be made to your account.",
+    verifyAccountUrl
+  );
+
+  return {
+    msg: "Email Sent",
+    info: info,
+    preview: nodemailer.getTestMessageUrl(info)
+  };
+}
+
+async function verifyAccountService(verificationToken: string) {
+  if (!verificationToken) {
+    throw new Error("Please enter a valid token");
+  }
+  const user = await User.findOne({
+    verificationToken: verificationToken,
+    verificationExpire: { $gt: Date.now() }
+  });
+  if (!user) {
+    throw new Error("Invalid Token or Session Expired");
+  }
+  user.isVerified = true;
+  user.verificationToken = undefined;
+  user.verificationExpire = undefined;
+  await user.save();
+  return "Account Verification Process Success";
+}
+
 export default {
   registerService,
   loginService,
-  forgotPasswordService
+  forgotPasswordService,
+  resetPasswordService,
+  editUserService,
+  validateEmailService,
+  verifyAccountService
 };
